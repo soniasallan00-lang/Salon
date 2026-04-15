f7265c54bc7c7888d2e6701e97b28c906e52f45e
Codecrafter2807 <codecrafter.2807@gmail.com>
2026-04-15 19:17:25 +0530

    Auto-create superuser on deploy

--- build.sh
+++ build.sh
@@ -4,3 +4,18 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Auto-create superuser from environment variables
+python manage.py shell << 'EOF'
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.environ.get('ADMIN_USERNAME', 'salon')
+password = os.environ.get('ADMIN_PASSWORD', 'admin1234')
+email    = os.environ.get('ADMIN_EMAIL', 'admin@salon.com')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f'Superuser "{username}" created.')
+else:
+    print(f'Superuser "{username}" already exists.')
+EOF
